7eed4f7da67f61c918734c2005234af9b382fe09
Justin Pillay <jpillay07@gmail.com>
2022-09-08 21:00:26 +0200

    Form completed first draft. Able to calculate dominant temperament

--- js/temperament-form.js
+++ js/temperament-form.js
@@ -1,12 +1,37 @@
+//Constants representing temperaments
+const SANGUINOUS = 0;
+const PHLEGMATIC = 1;
+const BILIOUS = 2;
+const MELANCHOLIC = 3;
+
+//Variable to store the sorted answers in descending order to determine dominant/subdominant temperament
+var sortedAnswers = [];
+
 //Get a nodelist of the next and prev buttons
 const nextBtns = document.querySelectorAll(".temperament-next-button");
 const prevBtns = document.querySelectorAll(".temperament-prev-button");
+const submitButton = document.querySelector(".temperament-submit-button");
+
+//Get the retake and book appointment buttons
+const retakeButton = document.querySelector(".temperament-reload-button");
+const bookButton = document.querySelector(".temperament-book-button");
 
 //Get a nodelist of all the form steps 
 const formSteps = document.querySelectorAll(".form-step");
 
+
+
 //Number to keep track of the current form step
 let formStepsNum = 0;
+
+//Event listener for booking button and reload buttons
+retakeButton.addEventListener("click", ()=>{
+    location.reload();
+});
+
+bookButton.addEventListener("click", ()=>{
+    window.open("https://clientportal.powerdiary.com/clientportal/thetibbdoctor", "_self");
+});
 
 //Event listener added to all next step buttons
 
@@ -14,6 +39,7 @@ nextBtns.forEach(btn => {
     btn.addEventListener("click", () => {
         formStepsNum++;
         updateFormSteps();
+        window.scrollTo(0, 0); // values are x,y-offset
     });
 });
 
@@ -21,6 +47,7 @@ prevBtns.forEach(btn => {
     btn.addEventListener("click", () => {
         formStepsNum--;
         updateFormSteps();
+        window.scrollTo(0, 0); // values are x,y-offset
     });
 });
 
@@ -32,4 +59,105 @@ function updateFormSteps() {
     });
 
     formSteps[formStepsNum].classList.add("form-step-active");
+    console.log(formSteps[formStepsNum]);
 }
+
+//count the amount of checked radio buttons and determine the temperament based on answers
+submitButton.addEventListener("click", ()=>{
+    const answerList = document.querySelectorAll('input[type="radio"]:checked');
+    const answerListArray = Array.from(answerList);
+
+    var answerObj = [
+        {
+            temperament: "sanguinous",
+            count: 0
+        },
+
+        {
+            temperament: "phlegmatic",
+            count: 0
+        },
+
+        {
+            temperament: "bilious",
+            count: 0
+        },
+
+        {
+            temperament: "melancholic",
+            count: 0
+        }
+    ];
+
+    answerListArray.forEach((answer) =>{
+        switch(answer.value){
+            case "sanguinous":
+                answerObj[SANGUINOUS].count += 1;
+                break;
+            case "phlegmatic":
+                answerObj[PHLEGMATIC].count += 1;
+                break;
+            case "bilious":
+                answerObj[BILIOUS].count += 1;
+                break;
+            default:
+                answerObj[MELANCHOLIC].count +=1;
+        }
+    });
+
+    sortedAnswers = answerObj.sort((a,b)=>{
+        if(a.count > b.count) return -1;
+        else return 1;
+    })
+
+    console.log(sortedAnswers);
+
+    const dominantIcon = document.querySelector(".dominant-icon");
+    const dominantText = document.querySelector(".dominant-text");
+    
+
+    if(sortedAnswers[0].temperament == "sanguinous"){
+        dominantIcon.src = "images/samguine.png";
+        const temperamentDescription = document.querySelector(".sanguinous-description");
+        dominantText.textContent ="Sanguinous";
+
+        temperamentDescription.classList.add("description-active");
+    }
+
+    if(sortedAnswers[0].temperament == "phlegmatic"){
+        dominantIcon.src = "images/phlegmatic.png";
+        const temperamentDescription = document.querySelector(".phlegmatic-description");
+        dominantText.textContent ="Phlegmatic";
+
+        temperamentDescription.classList.add("description-active");
+    }
+
+    if(sortedAnswers[0].temperament == "bilious"){
+        dominantIcon.src = "images/bilious.png";
+        const temperamentDescription = document.querySelector(".bilious-description");
+        dominantText.textContent ="Bilious";
+
+        temperamentDescription.classList.add("description-active");
+    }
+
+    if(sortedAnswers[0].temperament == "melancholic"){
+        dominantIcon.src = "images/melancholic.png";
+        const temperamentDescription = document.querySelector(".melancholic-description");
+        dominantText.textContent ="Melancholic";
+
+        temperamentDescription.classList.add("description-active");
+    }
+
+    const resultsDiv =  document.querySelector(".temperament-results");
+
+    formSteps[formStepsNum].classList.remove("form-step-active");
+
+    window.scrollTo(0, 0); // values are x,y-offset
+
+    resultsDiv.classList.add("temperament-results-active");
+
+    resultsDiv.classList.remove("temperament-results");
+
+
+
+});
